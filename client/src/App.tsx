dbc6aa8dda5d82a3a685b5887f1fbec91a8ea813
OlehLazar <oleh.lazar.2005@gmail.com>
2024-12-30 12:37:30 +0200

    added LoginPage to Routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,14 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import DefaultLayout from "./components/shared/DefaultLayout";
-import HomePage from "./pages/HomePage"
+import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
 
 const App = () => (
   <Router>
     <Routes>
       <Route element={<DefaultLayout />}>
         <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
       </Route>
     </Routes>
   </Router>
