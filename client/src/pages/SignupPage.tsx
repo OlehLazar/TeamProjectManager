c041173505dafad89be96a32a2af28701d8ff937
OlehLazar <oleh.lazar.2005@gmail.com>
2024-12-31 12:58:48 +0200

    completed SignupPage

--- client/src/pages/SignupPage.tsx
+++ client/src/pages/SignupPage.tsx
@@ -1,6 +1,16 @@
+import Input from "../components/shared/Input"
+import Button from "../components/shared/Button"
+
 const SignupPage = () => {
   return (
-    <div>SignupPage</div>
+    <div className="pt-10 pb-10 flex flex-col justify-center text-center gap-5 mx-auto w-1/2">
+        <h1 className="text-2xl font-bold text">Enter your data:</h1>
+        <Input name="nickname" placeholder="nickname" width="w-1/3" />
+        <Input name="email" placeholder="email" width="w-1/3" />
+        <Input name="password" placeholder="password" width="w-1/3" />
+        <Input name="repeat_password" placeholder="repeat password" width="w-1/3" />
+        <Button width="w-1/6">Sign up</Button>
+    </div>
   )
 }
 
