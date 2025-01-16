e47dc4603ee14f40c558ceb0b7a58408fdadf9ae
OlehLazar <oleh.lazar.2005@gmail.com>
2025-01-16 12:03:04 +0200

    implemented ValidationResult class, ValidateName, ValidateUserName, and ValidateAvatar methods in UserValidator

--- TeamProjectManager/TeamProjectManager.API/Validation/UserValidator.cs
+++ TeamProjectManager/TeamProjectManager.API/Validation/UserValidator.cs
@@ -17,12 +17,32 @@ public static class UserValidator
 
 	public static ValidationResult ValidateName(string name)
 	{
-		throw new NotImplementedException();
+		if (string.IsNullOrWhiteSpace(name))
+		{
+			return new ValidationResult(false, "Name cannot be empty.");
+		}
+
+		if (name.Length < 2)
+		{
+			return new ValidationResult(false, "Name must be at least 2 characters long.");
+		}
+
+		return new ValidationResult(true);
 	}
 
 	public static ValidationResult ValidateUserName(string userName)
 	{
-		throw new NotImplementedException();
+		if (string.IsNullOrWhiteSpace(userName))
+		{
+			return new ValidationResult(false, "Username cannot be empty.");
+		}
+
+		if (userName.Length < 5)
+		{
+			return new ValidationResult(false, "Username must be at least 5 characters long.");
+		}
+
+		return new ValidationResult(true);
 	}
 
 	public static ValidationResult ValidatePassword(string password)
@@ -32,6 +52,11 @@ public static class UserValidator
 
 	public static ValidationResult ValidateAvatar(string avatar)
 	{
-		throw new NotImplementedException();
+		if (!Uri.TryCreate(avatar, UriKind.Absolute, out _))
+		{
+			return new ValidationResult(false, "Avatar must be a valid URL.");
+		}
+
+		return new ValidationResult(true);
 	}
 }
--- TeamProjectManager/TeamProjectManager.API/obj/Debug/net8.0/TeamProjectManager.API.AssemblyInfo.cs
+++ TeamProjectManager/TeamProjectManager.API/obj/Debug/net8.0/TeamProjectManager.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TeamProjectManager.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e5355969f2165c3a16c0aa803f5d32a6dc72a216")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+19d6d187d3abc326ae950e19f73088cae653a31a")]
 [assembly: System.Reflection.AssemblyProductAttribute("TeamProjectManager.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TeamProjectManager.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
