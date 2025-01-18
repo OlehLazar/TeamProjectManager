301887677ce37364f6e4bf112a673287a0bd4345
OlehLazar <oleh.lazar.2005@gmail.com>
2025-01-18 13:41:08 +0200

    added try-catch to every method in UserController and implemented Logout

--- TeamProjectManager/TeamProjectManager.API/Controllers/UserController.cs
+++ TeamProjectManager/TeamProjectManager.API/Controllers/UserController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using TeamProjectManager.API.DTOs.User;
 using TeamProjectManager.API.Utilities;
 using TeamProjectManager.BLL.Interfaces;
+using TeamProjectManager.BLL.Validation;
 
 namespace TeamProjectManager.API.Controllers;
 
@@ -23,47 +24,97 @@ public class UserController : ControllerBase
 	[HttpPost("login")]
 	public async Task<IActionResult> Login(LoginDto loginDto)
 	{
-		
+		try
+		{
+
+		}
+		catch (AppException ex)
+		{
+			return BadRequest(ex.Message);
+		}
 	}
 
 	[HttpPost("register")]
 	public async Task<IActionResult> Register(RegisterDto registerDto)
 	{
+		try
+		{
 
+		}
+		catch (AppException ex)
+		{
+			return BadRequest(ex.Message);
+		}
 	}
 
 	[Authorize]
 	[HttpPost("logout")]
 	public async Task<IActionResult> Logout()
 	{
-
+		try
+		{
+			await _userService.LogoutUserAsync();
+			return Ok();
+		}
+		catch (AppException ex)
+		{
+			return BadRequest(ex.Message);
+		}
 	}
 
 	[Authorize]
 	[HttpGet("profile")]
 	public async Task<IActionResult> GetProfile()
 	{
+		try
+		{
 
+		}
+		catch (AppException ex)
+		{
+			return BadRequest(ex.Message);
+		}
 	}
 
 	[Authorize]
 	[HttpPut]
 	public async Task<IActionResult> UpdateProfile(UpdateProfileDto updateProfileDto)
 	{
+		try
+		{
 
+		}
+		catch (AppException ex)
+		{
+			return BadRequest(ex.Message);
+		}
 	}
 
 	[Authorize]
 	[HttpPut("change-password")]
 	public async Task<IActionResult> ChangePassword(string password)
 	{
+		try
+		{
 
+		}
+		catch (AppException ex)
+		{
+			return BadRequest(ex.Message);
+		}
 	}
 
 	[Authorize]
 	[HttpDelete]
 	public async Task<IActionResult> DeleteProfile()
 	{
+		try
+		{
 
+		}
+		catch (AppException ex)
+		{
+			return BadRequest(ex.Message);
+		}
 	}
 }
