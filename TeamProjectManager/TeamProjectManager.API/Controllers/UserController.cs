d0d107f9317b54ba69e78637bdf16f617717c009
OlehLazar <oleh.lazar.2005@gmail.com>
2025-01-18 13:35:57 +0200

    updated UserController

--- TeamProjectManager/TeamProjectManager.API/Controllers/UserController.cs
+++ TeamProjectManager/TeamProjectManager.API/Controllers/UserController.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using TeamProjectManager.API.DTOs.User;
+using TeamProjectManager.API.Utilities;
 using TeamProjectManager.BLL.Interfaces;
 
 namespace TeamProjectManager.API.Controllers;
@@ -9,40 +11,58 @@ namespace TeamProjectManager.API.Controllers;
 public class UserController : ControllerBase
 {
 	private readonly IUserService _userService;
-	private readonly IConfiguration _configuration;
 
-    public UserController(IUserService userService, IConfiguration configuration)
+	private readonly JwtHelper _jwtHelper;
+
+	public UserController(IUserService userService, JwtHelper jwtHelper)
     {
 		_userService = userService;
-		_configuration = configuration;
-    }
+		_jwtHelper = jwtHelper;
+	}
 
+	[HttpPost("login")]
 	public async Task<IActionResult> Login(LoginDto loginDto)
 	{
 		
 	}
 
+	[HttpPost("register")]
 	public async Task<IActionResult> Register(RegisterDto registerDto)
 	{
 
 	}
 
+	[Authorize]
+	[HttpPost("logout")]
 	public async Task<IActionResult> Logout()
 	{
 
 	}
 
+	[Authorize]
+	[HttpGet("profile")]
 	public async Task<IActionResult> GetProfile()
 	{
 
 	}
 
+	[Authorize]
+	[HttpPut]
 	public async Task<IActionResult> UpdateProfile(UpdateProfileDto updateProfileDto)
 	{
 
 	}
 
+	[Authorize]
+	[HttpPut("change-password")]
 	public async Task<IActionResult> ChangePassword(string password)
+	{
+
+	}
+
+	[Authorize]
+	[HttpDelete]
+	public async Task<IActionResult> DeleteProfile()
 	{
 
 	}
