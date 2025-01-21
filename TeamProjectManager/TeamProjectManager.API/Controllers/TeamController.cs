1341e92124bb060b5893f2a9debdf5acef0da10b
OlehLazar <oleh.lazar.2005@gmail.com>
2025-01-21 13:52:03 +0200

    created TeamController's methods templates

--- TeamProjectManager/TeamProjectManager.API/Controllers/TeamController.cs
+++ TeamProjectManager/TeamProjectManager.API/Controllers/TeamController.cs
@@ -1,9 +1,49 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using TeamProjectManager.API.DTOs.Team;
+using TeamProjectManager.BLL.Interfaces;
 
 namespace TeamProjectManager.API.Controllers;
 
 [ApiController]
 [Route("api/[controller]")]
+[Authorize]
 public class TeamController : ControllerBase
 {
+	private readonly ITeamService _teamService;
+
+	public TeamController(ITeamService teamService)
+	{
+		_teamService = teamService;
+	}
+
+	[HttpGet]
+	public async Task<IActionResult> GetTeams(int userId)
+	{
+		throw new NotImplementedException();
+	}
+
+	[HttpGet("{id}")]
+	public async Task<IActionResult> GetTeamById(int id)
+	{
+		throw new NotImplementedException();
+	}
+
+	[HttpPost]
+	public async Task<IActionResult> CreateTeam(CreateTeamDto createTeamDto)
+	{
+		throw new NotImplementedException();
+	}
+
+	[HttpPut("{id}")]
+	public async Task<IActionResult> UpdateTeam(FullTeamDto updateTeamDto)
+	{
+		throw new NotImplementedException();
+	}
+
+	[HttpDelete("{id}")]
+	public async Task<IActionResult> DeleteTeam(int id)
+	{
+		throw new NotImplementedException();
+	}
 }
