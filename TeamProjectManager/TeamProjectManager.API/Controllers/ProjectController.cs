b1df205d33a7fb99caf3d4b25b418bdcf404de7f
OlehLazar <oleh.lazar.2005@gmail.com>
2025-01-29 16:06:33 +0200

    did some work on ProjectController

--- TeamProjectManager/TeamProjectManager.API/Controllers/ProjectController.cs
+++ TeamProjectManager/TeamProjectManager.API/Controllers/ProjectController.cs
@@ -1,7 +1,10 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using TeamProjectManager.API.DTOs.Board;
 using TeamProjectManager.API.DTOs.Project;
+using TeamProjectManager.API.Validation;
 using TeamProjectManager.BLL.Interfaces;
+using TeamProjectManager.BLL.Models;
 using TeamProjectManager.BLL.Validation;
 
 namespace TeamProjectManager.API.Controllers;
@@ -38,6 +41,70 @@ public class ProjectController : ControllerBase
 		catch (Exception ex)
 		{
 			return StatusCode(500, new { message = "An unexpected error occurred.", error = ex.Message });
+		}
+	}
+
+	[HttpGet("{id}")]
+	public async Task<IActionResult> GetProjectById(int id)
+	{
+		try
+		{
+			var project = await _projectService.GetProjectByIdAsync(id);
+			if (project == null)
+			{
+				return NotFound("Project not found");
+			}
+
+			var projectDto = new FullProjectDto
+			(
+				project.Id,
+				project.Name,
+				project.Description,
+				project.TeamId,
+				project.Boards.Select(b => new BoardDto(b.Id, b.Name, b.Description, b.CreatedDate, b.ProjectId)).ToList()
+			);
+
+			return Ok(projectDto);
+		}
+		catch (AppException ex)
+		{
+			return BadRequest(ex.Message);
+		}
+		catch (Exception ex)
+		{
+			return StatusCode(500, new { message = "An unexpected error occured.", error = ex.Message });
+		}
+	}
+
+	[HttpPost]
+	public async Task<IActionResult> CreateProject(CreateProjectDto createProjectDto)
+	{
+		try
+		{
+			var validationResult = await new CreateProjectValidator().ValidateAsync(createProjectDto);
+			if (!validationResult.IsValid)
+			{
+				return BadRequest(validationResult.Errors);
+			}
+
+			var projectModel = new ProjectModel
+			{
+				Name = createProjectDto.Name,
+				Description = createProjectDto.Description,
+				TeamId = createProjectDto.TeamId,
+				Boards = []
+			};
+
+			await _projectService.AddProjectAsync(projectModel);
+			return Ok();
+		}
+		catch (AppException ex)
+		{
+			return BadRequest(ex.Message);
+		}
+		catch (Exception ex)
+		{
+			return StatusCode(500, new { message = "An unexpected error occured.", error = ex.Message });
 		}
 	}
 }
