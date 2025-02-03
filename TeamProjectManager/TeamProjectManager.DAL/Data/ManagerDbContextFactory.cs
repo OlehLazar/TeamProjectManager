ca55ba0e1d5f226da8ee76e444756bb51706b6d1
OlehLazar <oleh.lazar.2005@gmail.com>
2025-02-03 16:25:00 +0200

    changed data source path to relative

--- TeamProjectManager/TeamProjectManager.DAL/Data/ManagerDbContextFactory.cs
+++ TeamProjectManager/TeamProjectManager.DAL/Data/ManagerDbContextFactory.cs
@@ -6,7 +6,7 @@ public class ManagerDbContextFactory : IDesignTimeDbContextFactory<ManagerDbCont
 	public ManagerDbContext CreateDbContext(string[] args)
 	{
 		var options = new DbContextOptionsBuilder<ManagerDbContext>()
-			.UseSqlite("Data Source=C:\\Projects\\TeamProjectManager\\TeamProjectManager\\Db\\Manager.db")
+			.UseSqlite("Data Source=..\\Db\\Manager.db")
 			.Options;
 		return new ManagerDbContext(options);
 	}
