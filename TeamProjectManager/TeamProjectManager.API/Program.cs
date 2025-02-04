231b0194f00e38ad814d55f6f0d673e0a8bc077f
OlehLazar <oleh.lazar.2005@gmail.com>
2025-02-04 14:35:41 +0200

    completed API by updating Program.cs

--- TeamProjectManager/TeamProjectManager.API/Program.cs
+++ TeamProjectManager/TeamProjectManager.API/Program.cs
@@ -1,29 +1,116 @@
 using FluentValidation;
 using FluentValidation.AspNetCore;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using System.Text;
 using TeamProjectManager.API.Middleware;
+using TeamProjectManager.API.Utilities;
 using TeamProjectManager.API.Validation.User;
+using TeamProjectManager.BLL.Interfaces;
+using TeamProjectManager.BLL.Services;
 using TeamProjectManager.DAL.Data;
+using TeamProjectManager.DAL.Entities;
 using TeamProjectManager.DAL.Interfaces;
 
 var builder = WebApplication.CreateBuilder(args);
-
-builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
-
-builder.Services.AddControllers();
-
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-builder.Services.AddFluentValidationAutoValidation()
-				.AddFluentValidationClientsideAdapters();
-
-builder.Services.AddValidatorsFromAssemblyContaining<LoginValidator>();
 
 builder.Services.AddDbContext<ManagerDbContext>(options =>
 {
 	options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+
+builder.Services.AddIdentity<User, IdentityRole>(options =>
+{
+	options.Password.RequireDigit = false;
+	options.Password.RequireLowercase = false;
+	options.Password.RequireUppercase = false;
+	options.Password.RequireNonAlphanumeric = false;
+	options.Password.RequiredLength = 4;
+})
+	.AddEntityFrameworkStores<ManagerDbContext>()
+	.AddDefaultTokenProviders();
+
+var jwtSection = builder.Configuration.GetSection("Jwt");
+var key = Encoding.UTF8.GetBytes(jwtSection["Secret"]!);
+
+builder.Services
+	.AddAuthentication(options =>
+	{
+		options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+		options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+	})
+	.AddJwtBearer(options =>
+	{
+		options.TokenValidationParameters = new TokenValidationParameters
+		{
+			ValidateIssuer = true,
+			ValidateAudience = true,
+			ValidateLifetime = true,
+			ValidateIssuerSigningKey = true,
+			ValidIssuer = jwtSection["Issuer"],
+			ValidAudience = jwtSection["Audience"],
+			IssuerSigningKey = new SymmetricSecurityKey(key),
+		};
+	});
+
+builder.Services.AddCors(options =>
+{
+	options.AddDefaultPolicy(opt =>
+	{
+		opt.AllowAnyOrigin()
+			.AllowAnyMethod()
+			.AllowAnyHeader();
+	});
+});
+
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+builder.Services.AddScoped<IUserService, UserService>();
+builder.Services.AddScoped<ITeamService, TeamService>();
+builder.Services.AddScoped<IProjectService, ProjectService>();
+builder.Services.AddScoped<IBoardService, BoardService>();
+builder.Services.AddScoped<ITaskService, TaskService>();
+builder.Services.AddScoped<INotificationService, NotificationService>();
+builder.Services.AddScoped<JwtHelper>();
+
+builder.Services.AddControllers();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+	c.SwaggerDoc("v1", new OpenApiInfo { Title = "Your API Title", Version = "v1" });
+
+	c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+	{
+		Description = "JWT Authorization header using the Bearer scheme. Example: \"Authorization: Bearer {token}\"",
+		Name = "Authorization",
+		In = ParameterLocation.Header,
+		Type = SecuritySchemeType.ApiKey,
+		Scheme = "Bearer",
+	});
+
+	c.AddSecurityRequirement(new OpenApiSecurityRequirement
+	{
+		{
+			new OpenApiSecurityScheme
+			{
+				Reference = new OpenApiReference
+				{
+					Type = ReferenceType.SecurityScheme,
+					Id = "Bearer",
+				},
+			},
+			Array.Empty<string>()
+		},
+	});
+});
+
+builder.Services.AddFluentValidationAutoValidation()
+				.AddFluentValidationClientsideAdapters();
+
+builder.Services.AddValidatorsFromAssemblyContaining<LoginValidator>();
 
 var app = builder.Build();
 
@@ -35,8 +122,10 @@ if (app.Environment.IsDevelopment())
 
 app.UseMiddleware<ErrorHandler>();
 
+app.UseCors();
 app.UseHttpsRedirection();
-
+app.UseAuthentication();
 app.UseAuthorization();
 app.MapControllers();
+
 app.Run();
--- TeamProjectManager/TeamProjectManager.BLL/Utilities/FilterBuilder.cs
+++ TeamProjectManager/TeamProjectManager.BLL/Utilities/FilterBuilder.cs
@@ -1,44 +1,44 @@
-﻿using System.Linq.Expressions;
-using TeamProjectManager.BLL.Models;
+﻿//using System.Linq.Expressions;
+//using TeamProjectManager.BLL.Models;
 
-namespace TeamProjectManager.BLL.Utilities;
+//namespace TeamProjectManager.BLL.Utilities;
 
-public static class FilterBuilder
-{
-	public static IQueryable<T> ApplyFilters<T>(IQueryable<T> query,FilterModel filter,
-	params Expression<Func<T, bool>>[] filterExpressions)
-	{
-		foreach (var filterExpression in filterExpressions)
-		{
-			query = query.Where(filterExpression);
-		}
+//public static class FilterBuilder
+//{
+//	public static IQueryable<T> ApplyFilters<T>(IQueryable<T> query,FilterModel filter,
+//	params Expression<Func<T, bool>>[] filterExpressions)
+//	{
+//		foreach (var filterExpression in filterExpressions)
+//		{
+//			query = query.Where(filterExpression);
+//		}
 
-		if (!string.IsNullOrWhiteSpace(filter.SortBy))
-		{
-			query = ApplySorting(query, filter.SortBy, filter.IsDescending);
-		}
+//		if (!string.IsNullOrWhiteSpace(filter.SortBy))
+//		{
+//			query = ApplySorting(query, filter.SortBy, filter.IsDescending);
+//		}
 
-		if (filter.Page > 0 && filter.PageSize > 0)
-		{
-			query = query.Skip((filter.Page - 1) * filter.PageSize)
-						 .Take(filter.PageSize);
-		}
+//		if (filter.Page > 0 && filter.PageSize > 0)
+//		{
+//			query = query.Skip((filter.Page - 1) * filter.PageSize)
+//						 .Take(filter.PageSize);
+//		}
 
-		return query;
-	}
+//		return query;
+//	}
 
-	private static IQueryable<T> ApplySorting<T>(IQueryable<T> query, string sortBy, bool isDescending)
-	{
-		var param = Expression.Parameter(typeof(T), "entity");
-		var property = Expression.Property(param, sortBy);
-		var keySelector = Expression.Lambda(property, param);
+//	private static IQueryable<T> ApplySorting<T>(IQueryable<T> query, string sortBy, bool isDescending)
+//	{
+//		var param = Expression.Parameter(typeof(T), "entity");
+//		var property = Expression.Property(param, sortBy);
+//		var keySelector = Expression.Lambda(property, param);
 
-		var methodName = isDescending ? "OrderByDescending" : "OrderBy";
-		var method = typeof(Queryable).GetMethods()
-			.First(m => m.Name == methodName && m.GetParameters().Length == 2);
+//		var methodName = isDescending ? "OrderByDescending" : "OrderBy";
+//		var method = typeof(Queryable).GetMethods()
+//			.First(m => m.Name == methodName && m.GetParameters().Length == 2);
 
-		var genericMethod = method.MakeGenericMethod(typeof(T), property.Type);
+//		var genericMethod = method.MakeGenericMethod(typeof(T), property.Type);
 
-		return (IQueryable<T>)genericMethod.Invoke(null, new object[] { query, keySelector });
-	}
-}
+//		return (IQueryable<T>)genericMethod.Invoke(null, new object[] { query, keySelector });
+//	}
+//}
