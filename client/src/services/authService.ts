b94e8bd2a604d3374b35c43f5e3e333da53ed806
OlehLazar <oleh.lazar.2005@gmail.com>
2025-02-05 20:15:09 +0200

    added axios for API requests and implemented authentication service

---
+++ client/src/services/authService.ts
@@ -0,0 +1,36 @@
+import api from "./api";
+
+interface LoginData {
+  userName: string;
+  password: string;
+}
+
+interface RegisterData {
+  firstName: string;
+  lastName: string;
+  userName: string;
+  password: string;
+}
+
+export const login = async (data: LoginData) => {
+  const response = await api.post('/auth/login', data);
+  localStorage.setItem('token', response.data.token);
+  return response.data;
+};
+
+export const register = async (data: RegisterData) => {
+  const response = await api.post('/auth/register', data);
+  return response.data;
+};
+
+export const logout = async () => {
+  const token = localStorage.getItem('token');
+  if (token) {
+    await api.post('/auth/logout', {}, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    localStorage.removeItem('token');
+  }
+};
