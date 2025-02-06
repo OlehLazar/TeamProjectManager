549f607530cfed9580cfb452a17b17b27a363cc0
OlehLazar <oleh.lazar.2005@gmail.com>
2025-02-06 17:08:35 +0200

    implemented SignupForm component for user registration

---
+++ client/src/components/signupPage/SignupForm.tsx
@@ -0,0 +1,48 @@
+import { useState } from "react";
+import Input from "../shared/Input";
+import Button from "../shared/Button";
+import { register } from "../../services/authService"; // Import API call function
+
+const SignupForm: React.FC = () => {
+  const [formData, setFormData] = useState({
+    firstName: "",
+    lastName: "",
+    userName: "",
+    password: "",
+    repeatPassword: "",
+  });
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    
+    if (formData.password !== formData.repeatPassword) {
+      alert("Passwords do not match!");
+      return;
+    }
+
+    try {
+      await register(formData);
+      window.location.href = "/login";
+    } catch (error) {
+      console.error("Signup failed:", error);
+      alert(error);
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className="flex flex-col items-center gap-4">
+      <Input name="firstName" placeholder="First Name" width="w-1/3" value={formData.firstName} onChange={handleChange} />
+      <Input name="lastName" placeholder="Last Name" width="w-1/3" value={formData.lastName} onChange={handleChange} />
+      <Input name="userName" placeholder="Nickname" width="w-1/3" value={formData.userName} onChange={handleChange} />
+      <Input name="password" type="password" placeholder="Password" width="w-1/3" value={formData.password} onChange={handleChange} />
+      <Input name="repeatPassword" type="password" placeholder="Repeat Password" width="w-1/3" value={formData.repeatPassword} onChange={handleChange} />
+      <Button width="w-1/6" type="submit">Sign up</Button>
+    </form>
+  );
+};
+
+export default SignupForm;
--- client/src/pages/SignupPage.tsx
+++ client/src/pages/SignupPage.tsx
@@ -1,17 +1,10 @@
-import Input from "../components/shared/Input"
-import Button from "../components/shared/Button"
+import SignupForm from "../components/signupPage/SignupForm"
 
 const SignupPage = () => {
   return (
     <div className="pt-10 pb-10 flex flex-col justify-center text-center gap-5 mx-auto w-1/2">
         <h1 className="text-2xl font-bold text font-ptSerif">Enter your data:</h1>
-        <Input name="first_name" placeholder="first name" width="w-1/3" />
-        <Input name="last_name" placeholder="last name" width="w-1/3" />
-        <Input name="nickname" placeholder="nickname" width="w-1/3" />
-        <Input name="email" placeholder="email" width="w-1/3" />
-        <Input name="password" placeholder="password" width="w-1/3" />
-        <Input name="repeat_password" placeholder="repeat password" width="w-1/3" />
-        <Button width="w-1/6">Sign up</Button>
+        <SignupForm />
     </div>
   )
 }
