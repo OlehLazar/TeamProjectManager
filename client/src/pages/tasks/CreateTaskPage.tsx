ad4ae0253b071dbf105ffb623f9db54e4f7b86bd
OlehLazar <oleh.lazar.2005@gmail.com>
2025-03-03 21:29:53 +0200

    completed CreateTaskPage

--- client/src/pages/tasks/CreateTaskPage.tsx
+++ client/src/pages/tasks/CreateTaskPage.tsx
@@ -8,6 +8,8 @@ import { useParams } from "react-router-dom";
 import { getMembers } from "../../services/teamService";
 import { UserDto } from "../../interfaces/dtos/UserDto";
 import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+import ErrorMessage from "../../components/ui/ErrorMessage";
 
 type FormFields = {
     name: string;
@@ -29,16 +31,46 @@ const CreateTaskPage = () => {
     const { register, handleSubmit, control, formState: { errors }, setError } = useForm<FormFields>();
 
     const onSubmit: SubmitHandler<FormFields> = async (data) => {
-        await createTask({name: data.name, description: data.description, startDate: data.startDate, endDate: data.endDate, boardId: boardId, assigneeUsername: data.assigneeUsername})
+        try {
+            await createTask({name: data.name, description: data.description, startDate: data.startDate, endDate: data.endDate, boardId: boardId, assigneeUsername: data.assigneeUsername})
+        } catch (error) {
+            if (axios.isAxiosError(error)) {
+                const fieldMapping: { [key: string]: keyof FormFields } = {
+                    Name: 'name',
+                    Description: 'description',
+                    StartDate: 'startDate',
+                    EndDate: 'endDate',
+                    Assignee: 'assigneeUsername',
+                };
+
+                const serverErrors = error.response?.data?.errors;
+                Object.entries(serverErrors).forEach(([field, messages]) => {
+                    const formField = fieldMapping[field];
+                    const errorMessage = Array.isArray(messages)
+                        ? messages.join(" ")
+                        : (messages as string);
+
+                    if (formField) {
+                        setError(formField, {
+                            type: "server",
+                            message: errorMessage
+                        });
+                    }
+                });
+            }
+        }
     };
 
     return (
-        <div>
+        <div className="pt-10 pb-10 flex flex-col">
             <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-5 text-center w-1/2 mx-auto">
+                <h1 className="font-ptSerif text-2xl">Enter your data</h1>
+
                 <Input {...register("name", { required: "Name is required" })} placeholder="Name" />
-                {errors.name && <span className="text-red-500">{errors.name.message}</span>}
+                {errors.name && <ErrorMessage message={errors.name.message!} />}
 
                 <Input {...register("description")} placeholder="Description" />
+                {errors.description && <ErrorMessage message={errors.description.message!} />}
 
                 <Controller
                     control={control}
@@ -53,7 +85,7 @@ const CreateTaskPage = () => {
                         />
                     )}
                 />
-                {errors.startDate && <span className="text-red-500">{errors.startDate.message}</span>}
+                {errors.startDate && <ErrorMessage message={errors.startDate.message!} />}
 
                 <Controller
                     control={control}
@@ -68,7 +100,7 @@ const CreateTaskPage = () => {
                         />
                     )}
                 />
-                {errors.endDate && <span className="text-red-500">{errors.endDate.message}</span>}
+                {errors.endDate && <ErrorMessage message={errors.endDate.message!} />}
                 
                 <Controller
                     control={control}
@@ -78,7 +110,7 @@ const CreateTaskPage = () => {
                         <select
                             {...field}
                             onChange={(e) => field.onChange(e.target.value)}
-                            className="w-full p-2 border rounded"
+                            className="w-1/4 p-2 border rounded mx-auto"
                         >
                             <option value="">Select assignee</option>
                             {isLoading ? (
@@ -93,6 +125,7 @@ const CreateTaskPage = () => {
                         </select>
                     )}
                 />
+                {errors.assigneeUsername && <ErrorMessage message={errors.assigneeUsername.message!} />}
 
                 <Button type="submit" width="w-1/5">Create</Button>
             </form>
